440c1c4ff65a0eebb15c3286ff2489f34bed79b0
Mopper <moppper@proton.me>
2026-03-02 04:20:40 -0500

    Update message in new-feature branch

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return jsonify({
-        "message": "Hello from Flask in Docker & Kubernetes!",
+        "message": "Hello from Flask in Docker & Kubernetes NEW FEATURE branch!",
         "message": "New Branch",
         "pod": os.environ.get("MY_POD", "unknown")
     })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
